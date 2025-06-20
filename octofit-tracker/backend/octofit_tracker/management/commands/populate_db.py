6868c2d1649f9d2d6066c3ecba7b4bdbd743674c
King Paul Pulgares <53598739+kengpul@users.noreply.github.com>
2025-06-20 08:40:12 +0000

    Add clarification comment for test data population script

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from django.utils import timezone
 
+# This script populates test data for the octofit_db database.
+
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data.'
 
